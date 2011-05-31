5a1a0ea686cb08f15778cd27397b4c8df073579e
lumpy <wzlxx.c@gmail.com>
2011-05-31 16:12:15 +0800

    一些更新

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,19 +1,19 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Start of xbindkeys configuration ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; This configuration is guile based.
-;;
-;; This configuration allow combo keys.
-;; ie Control+z Control+e -> xterm
-;;    Control+z z         -> rxvt
-;;    Control+z Control+g -> quit second mode
-;;
-;; It also allow to add or remove a key on the fly!
-
 
 (define (first-binding)
-  (xbindkey-function '(Control semicolon) prefix-key))
+  (xbindkey-function '(Control semicolon) prefix-key)
+  (xbindkey '(Mod4 Return) "~/.bin/rxvt")
+  (xbindkey '(Mod4 i) "xterm")
+  (xbindkey '(Control Mod4 l) "slock")
+  (xbindkey '(Print) "scrot -u -e 'mv $f ~/picture/shot/'")
+  (xbindkey '(Mod4 Print) "scrot -s -e 'mv $f ~/picture/shot/'")
+  (xbindkey '(Mod4 Left) "mocp -G")
+  (xbindkey '(Mod4 Right) "mocp -f")
+  (xbindkey '(Mod4 Up) "~/.bin/moc")
+  (xbindkey '(Mod4 Down) "mocp -t n")
+  )
 
+;;--------------------jump-or-exec--------------------
 (define (reset-first-binding)
   (ungrab-all-keys)
   (remove-all-keys)
@@ -23,11 +23,7 @@
 (define (prefix-key)
   (ungrab-all-keys)
   (remove-all-keys)
-  (xbindkey-function 'p
-		     (lambda ()
-		       (run-command "wmctrl -xa XTerm || xterm -e screen")
-		       (reset-first-binding)))
-  (xbindkey-function 'Return
+  (xbindkey-function 'u
 		     (lambda ()
 		       (run-command "wmctrl -xa URxvt || ~/.bin/rxvt -e screen")
 		       (reset-first-binding)))
@@ -43,16 +39,8 @@
 		     (lambda ()
 		       (run-command "wmctrl -xa Thunar || thunar")
 		       (reset-first-binding)))
-  (xbindkey-function 'l
-		     (lambda ()
-		       (run-command "slock")
-		       (reset-first-binding)))
   (xbindkey-function '(control g) reset-first-binding)
   (debug)
   (grab-all-keys))
-
 (first-binding)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; End of xbindkeys configuration ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;--------------------jump-or-exec-end--------------------
